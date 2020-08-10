0f6bd80abf5da1624045c759bad28d08a460b0fe
Douglas Maringa <douglasmaringa4@gmail.com>
2020-08-10 16:21:29 +0200

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyDUpuZj1XcK_QN-M7L206wLiVIFQmp75xY",
-    authDomain: "image-330a5.firebaseapp.com",
-    databaseURL: "https://image-330a5.firebaseio.com",
-    projectId: "image-330a5",
-    storageBucket: "image-330a5.appspot.com",
-    messagingSenderId: "644668823938",
-    appId: "1:644668823938:web:a4e6a1f32b4ad4d56a579b",
-    measurementId: "G-4E7H7083J7"
+    //Put your config file here
   };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
